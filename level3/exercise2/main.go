4c5db571ee761e609b3c951b3b824050afa39cbf
Amir Massarwa <amassarwi@gmail.com>
2019-08-08 23:44:32 +0200

    ninja level 3 - exrcise 2 print index

--- level3/exercise2/main.go
+++ level3/exercise2/main.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	for i := 65; i <= 90; i++ {
+		fmt.Printf("%v\n", i)
 		j := 0
 		for j < 3 {
-			fmt.Printf("%#U\n", i)
+			fmt.Printf("\t%#U\n", i)
 			j++
 		}
 	}
