dfcb792b6a6f293fcc5424f709694f1101d23378
Amir Massarwa <amassarwi@gmail.com>
2019-08-10 00:11:41 +0200

    level 4 - exercise 9

---
+++ level4/exercise9/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"James":           []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		"moneypenny_miss": []string{`James Bond`, `Literature`, `Computer Science`},
+		"no_dr":           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	m["amir"] = []string{`programming`, `taveling`, `blockchain`}
+
+	for k, values := range m {
+		fmt.Println(k, values)
+	}
+}
