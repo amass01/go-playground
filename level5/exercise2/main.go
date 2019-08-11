aad7694a3cad91bde17610fec7487a199156891b
Amir Massarwa <amassarwi@gmail.com>
2019-08-11 20:22:18 +0200

    level 5 - exercise 2

--- level5/exercise2/main.go
+++ level5/exercise2/main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	favIce    []string
+}
+
 func main() {
+
+	p1 := person{
+		"Amir",
+		"Massarwa",
+		[]string{"pink", "green"},
+	}
+
+	// explicit use of struct fields names when creating an instance
+	p2 := person{
+		firstName: "Amirs",
+		lastName:  "Massarwas",
+		favIce:    []string{"blue", "red"},
+	}
+
+	m := map[string]person{
+		p1.lastName: p1,
+		p2.lastName: p2,
+	}
+
+	for _, v := range m {
+		fmt.Println(v)
+	}
 
 }
