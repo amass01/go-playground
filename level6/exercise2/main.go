1664870d5ffa0d1dc8e988ad5f69751aaa7214f6
Amir Massarwa <amassarwi@gmail.com>
2019-08-12 23:02:18 +0200

    level 6 - exercise 2 - part 2

--- level6/exercise2/main.go
+++ level6/exercise2/main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("The sum of 1, 4, 6, 3, 6, 2, 3: is", foo(1, 4, 6, 3, 6, 2, 3))
+	fmt.Println("The sum of []int{1, 4, 6, 3, 6, 2, 3}: is", bar([]int{1, 4, 6, 3, 6, 2, 3}))
 }
 
 func foo(nums ...int) int {
+	s := 0
+	for _, v := range nums {
+		s += v
+	}
+	return s
+}
+
+func bar(nums []int) int {
 	s := 0
 	for _, v := range nums {
 		s += v
