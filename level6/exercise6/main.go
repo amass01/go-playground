705076d823102968555beb911bcc7b4b1af8c2bf
Amir Massarwa <amassarwi@gmail.com>
2019-08-13 00:13:14 +0200

    better level 6 - exercise 6

--- level6/exercise6/main.go
+++ level6/exercise6/main.go
@@ -3,18 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	f := idGenerator()
 
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-}
-
-func idGenerator() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
-	}
+	func() {
+		fmt.Println("awesome!!")
+	}()
 }
