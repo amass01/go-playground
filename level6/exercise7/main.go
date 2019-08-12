34b71ddc020fcfb466d2432bab459dc66ed57b60
Amir Massarwa <amassarwi@gmail.com>
2019-08-13 00:13:43 +0200

    level 6 - exercise 7

---
+++ level6/exercise7/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nextID := idGenerator()
+
+	fmt.Println(nextID())
+	fmt.Println(nextID())
+	fmt.Println(nextID())
+	fmt.Println(nextID())
+}
+
+func idGenerator() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
