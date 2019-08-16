a33cff80ac9c4dcb12fae60778ecf3208c825bdf
Amir Massarwa <amassarwi@gmail.com>
2019-08-16 22:37:30 +0200

    level 10 - exervcse 1 buffered channel

--- level10/exercise1/main.go
+++ level10/exercise1/main.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 )
 
-func main() {
-	c := make(chan int)
+// func main() {
+// 	c := make(chan int)
 
-	go func() {
-		c <- 42
-	}()
+// 	go func() {
+// 		c <- 42
+// 	}()
+
+// 	fmt.Println(<-c)
+// }
+
+func main() {
+	c := make(chan int, 1)
+
+	c <- 42
 
 	fmt.Println(<-c)
 }
