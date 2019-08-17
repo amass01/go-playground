b5b2eb140aee459a2659db20e311bce5d13c8fb2
Amir Massarwa <amassarwi@gmail.com>
2019-08-17 22:46:15 +0200

    added benchmark

--- level13/exercise1/dog/main_test.go
+++ level13/exercise1/dog/main_test.go
@@ -1,6 +1,11 @@
 package dog
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/amassarwi/go-playground/level12/dog"
+)
 
 func TestYears(t *testing.T) {
 	age := Years(10)
@@ -9,9 +14,21 @@ func TestYears(t *testing.T) {
 	}
 }
 
+func BenchmarkYears(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Years(10)
+	}
+}
+
 func TestYearsTwo(t *testing.T) {
 	age := YearsTwo(20)
 	if age != 140 {
 		t.Error("Expected:", 140, "got", age)
 	}
+}
+
+func ExampleYears() {
+	fmt.Println(dog.Years(10))
+	// outputs:
+	// 70
 }
