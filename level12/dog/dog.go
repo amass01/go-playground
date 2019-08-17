65eafac5474a759ea2eff5e353e4c9d4ae2505d8
Amir Massarwa <amassarwi@gmail.com>
2019-08-17 22:25:53 +0200

    fix dog calc

--- level12/dog/dog.go
+++ level12/dog/dog.go
@@ -4,5 +4,5 @@ package dog
 
 // Years takes human years and turns them into dog years (1 human year = 7 dog years).
 func Years(humanAge float64) float64 {
-	return humanAge / 7
+	return humanAge * 7
 }
